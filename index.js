f547b8a8ce6dcff69ed5d0a374199dece72e6a99
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-02-21 14:29:13 +0300

    Реализовал все фильтры

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (function () {
     var video = document.querySelector('.camera__video'),
-        canvas = document.querySelector('.camera__canvas');
+        canvas = document.querySelector('.camera__canvas'),
+        context = canvas.getContext('2d'),
+        back = document.createElement('canvas'),
+        backcontext = back.getContext('2d');
 
     var getVideoStream = function (callback) {
         navigator.getUserMedia = navigator.getUserMedia ||
@@ -61,29 +64,76 @@
         return filters[filterName](pixel);
     };
 
-    var applyFilter = function () {
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
-                var pixel = canvas.getContext('2d').getImageData(x, y, 1, 1);
+    var applyFilter = function (v,c,bc,w,h) {
+        // Отрисуем картинку на фоновом канвасе
+        bc.drawImage(v,0,0,w,h);
 
-                pixel.data = applyFilterToPixel(pixel.data);
+        // Полуаем информацию о пикселях с фонового канваса
+        var idata = bc.getImageData(0,0,w,h);
+        var data = idata.data;
 
-                canvas.getContext('2d').putImageData(pixel, x, y);
-            }
+        // Накладываем фильтр на пиксели в цикле
+        for(var i = 0; i < data.length; i+=4) {
+            // var r = data[i];
+            // var g = data[i+1];
+            // var b = data[i+2];
+            // var brightness = (3*r+4*g+b)>>>3;
+            // data[i] = brightness;
+            // data[i+1] = brightness;
+            // data[i+2] = brightness;
+
+            // Инверт
+            // var r = 255 - data[i];
+            // var g = 255 - data[i + 1];
+            // var b = 255 - data[i + 2];
+            // data[i] = r;
+            // data[i+1] = g;
+            // data[i+2] = b;
+
+            // Оттенки серого
+            // var r = data[i];
+            // var g = data[i + 1];
+            // var b = data[i + 2];
+            // var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            // data[i] = v;
+            // data[i+1] = v;
+            // data[i+2] = v;
+
+            // Черно-белый
+            // var r = data[i];
+            // var g = data[i + 1];
+            // var b = data[i + 2];
+            // var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= 128) ? 255 : 0;
+            // data[i] = v;
+            // data[i+1] = v;
+            // data[i+2] = v;
+
+
+            // applyFilterToPixel(data);
         }
+        idata.data = data;
+
+        // Отрисовываем результат на существующий канвас
+        c.putImageData(idata,0,0);
     };
 
     var captureFrame = function () {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+        cw = video.videoWidth;
+        ch = video.videoHeight;
 
-        canvas.getContext('2d').drawImage(video, 0, 0);
-        applyFilter();
+        canvas.width = cw;
+        canvas.height = ch;
+
+        back.width = cw;
+        back.height = ch;
+
+        applyFilter(video,context,backcontext,cw,ch);
     };
 
     getVideoStream(function () {
         captureFrame();
 
+        // setInterval(captureFrame, 16);
         setInterval(captureFrame, 16);
     });
 })();
